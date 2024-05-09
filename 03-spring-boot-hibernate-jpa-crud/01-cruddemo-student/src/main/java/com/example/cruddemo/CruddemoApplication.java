879b171b8d3ceff0e9aaed053538dc2648e3aa23
Mahdi <amiralmasi021@gmail.com>
2024-05-09 21:13:28 +0330

    Find students by lastname

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/example/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/example/cruddemo/CruddemoApplication.java
@@ -22,8 +22,18 @@ public class CruddemoApplication {
 //            createStudent(studentDAO);
 //            createMultipleStudents(studentDAO);
 //                readStudent(studentDAO);
-            queryForStudents(studentDAO);
+//            queryForStudents(studentDAO);
+            queryForStudentsByLastName(studentDAO);
         };
+    }
+
+    private void queryForStudentsByLastName(StudentDAO studentDAO) {
+        // get a list of students
+            List<Student> theStudents = studentDAO.findByLastName("Doe");
+        // display list of students
+        for(Student tempStudent: theStudents){
+            System.out.println(tempStudent);
+        }
     }
 
     private void queryForStudents(StudentDAO studentDAO) {
