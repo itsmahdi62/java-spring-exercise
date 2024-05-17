7dfb10f87b1c0c984cd5727cc024936fa6a09835
Mahdi <amiralmasi021@gmail.com>
2024-05-17 22:52:32 +0330

    Work with variable path

---
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/example/demo/rest/StudentRestController.java
@@ -0,0 +1,35 @@
+package com.example.demo.rest;
+
+import com.example.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+    private List<Student> theStudents;
+    // define @PostConstruct to load the student data ... only one!
+    @PostConstruct
+    public void loadData(){
+        theStudents = new ArrayList<>();
+        theStudents.add(new Student("Poornuma" , "Patel"));
+        theStudents.add(new Student("John" , "Almasi"));
+        theStudents.add(new Student("Melina" , "Chabok"));
+    }
+    // define endpoint for "/students" - return a list of students
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+
+        return  theStudents;
+    }
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId){
+        return theStudents.get(studentId);
+    }
+}
