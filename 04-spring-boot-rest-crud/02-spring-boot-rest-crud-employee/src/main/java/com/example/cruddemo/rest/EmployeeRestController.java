0fa64155f92b83a9803ca0f2c19b7a97e66b066b
Mahdi <amiralmasi021@gmail.com>
2024-05-24 00:04:50 +0330

    Add service layer to the project

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/example/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/example/cruddemo/rest/EmployeeRestController.java
@@ -2,6 +2,8 @@ package com.example.cruddemo.rest;
 
 import com.example.cruddemo.dao.EmployeeDAO;
 import com.example.cruddemo.entity.Employee;
+import com.example.cruddemo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +13,17 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
-    private EmployeeDAO employeeDAO ;
+    private EmployeeService employeeService;
+
+    @Autowired
     // quick and dirty: inject employee dao (use constructor injection)
-    public EmployeeRestController(EmployeeDAO theEmployeeDAO){
-        employeeDAO = theEmployeeDAO;
+    public EmployeeRestController(EmployeeService theEmployeeService) {
+        employeeService = theEmployeeService;
     }
+
     // expose "/employee" and return a list of employees
     @GetMapping("/employees")
-    public List<Employee> findAll(){
-        return employeeDAO.fineAll();
+    public List<Employee> findAll() {
+        return employeeService.findAll();
     }
 }
