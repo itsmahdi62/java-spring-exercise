e18c5f7c15dbc7e5c5f946436524964a6a8b78e9
Mahdi <amiralmasi021@gmail.com>
2024-05-24 23:52:20 +0330

    Create an employee route

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/example/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/example/cruddemo/rest/EmployeeRestController.java
@@ -4,9 +4,7 @@ import com.example.cruddemo.dao.EmployeeDAO;
 import com.example.cruddemo.entity.Employee;
 import com.example.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,4 +24,25 @@ public class EmployeeRestController {
     public List<Employee> findAll() {
         return employeeService.findAll();
     }
+
+    // add mapping for GET /employees/{employeeId}
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId){
+        Employee theEmployee = employeeService.findById(employeeId);
+        if (theEmployee == null){
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+        return theEmployee;
+    }
+
+    // add mapping for POST /employees - add new employees
+    @PostMapping("/employees/{employeeId}")
+    public Employee addEmployee(@RequestBody Employee theEmployee){
+        // also just in case the pass an id in json ... set id to 0
+        // this is to force a save of new item , instead of update
+        theEmployee.setId(0);
+        Employee dbEmployee = employeeService.save(theEmployee);
+        return dbEmployee;
+    }
+
 }
